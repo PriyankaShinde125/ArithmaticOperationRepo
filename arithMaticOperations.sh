200287211160b257b854d6387e952bc733a2c90c
“PriyankaShinde” <pkshinde125@gmail.com>
2022-10-16 09:48:55 +0530

    [Priyanka] integrated with UC7 stored results in array from dictionary

--- arithMaticOperations.sh
+++ arithMaticOperations.sh
@@ -1,6 +1,7 @@
 #! /bin/bash -x
 
 declare -A resultDict
+declare -a resultArr
 echo "enter three numbers"
 read a
 read b
@@ -10,3 +11,11 @@ resultDict['result2']=$((a * b + c))
 resultDict['result3']=$((c + a / b))
 resultDict['result4']=$((a % b + c))
 echo "${resultDict[@]}"
+
+j=0
+for i in ${!resultDict[@]}
+do
+	resultArr[$j]=${resultDict[$i]}
+	((j++))
+done
+echo "${resultArr[@]}"

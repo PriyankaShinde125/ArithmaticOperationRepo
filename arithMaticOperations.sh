ae6ec8d841b5c6f2a2393130e874493085c10805
“PriyankaShinde” <pkshinde125@gmail.com>
2022-10-16 09:42:15 +0530

    [Priyanka] integrated with UC6 stored results in dictionary

--- arithMaticOperations.sh
+++ arithMaticOperations.sh
@@ -1,14 +1,12 @@
 #! /bin/bash -x
 
+declare -A resultDict
 echo "enter three numbers"
 read a
 read b
 read c
-result1=$((a + b * c))
-result2=$((a * b + c))
-result3=$((c + a / b))
-result4=$((a % b + c))
-echo "a+b*c= $result1"
-echo "a*b+c= $result2"
-echo "c+a/b= $result3"
-echo "a%b+c= $result4"
+resultDict['result1']=$((a + b * c))
+resultDict['result2']=$((a * b + c))
+resultDict['result3']=$((c + a / b))
+resultDict['result4']=$((a % b + c))
+echo "${resultDict[@]}"

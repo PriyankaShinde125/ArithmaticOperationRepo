02fd426bd8512cc4eb14ca57e5d7e4f7fe120c62
“PriyankaShinde” <pkshinde125@gmail.com>
2022-10-16 09:35:36 +0530

    [Priyanka] integrated with UC5 compute equation 4

--- arithMaticOperations.sh
+++ arithMaticOperations.sh
@@ -7,6 +7,8 @@ read c
 result1=$((a + b * c))
 result2=$((a * b + c))
 result3=$((c + a / b))
+result4=$((a % b + c))
 echo "a+b*c= $result1"
 echo "a*b+c= $result2"
 echo "c+a/b= $result3"
+echo "a%b+c= $result4"
